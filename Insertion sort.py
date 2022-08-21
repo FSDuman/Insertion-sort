ed08af9f3d065bed5ca905343a61b5e341917aec
faruk selim <farukselimduman8@gmail.com>
2022-08-21 17:07:25 +0300

    'first commit'

---
+++ Insertion sort.py
@@ -0,0 +1,20 @@
+[22,27,16,2,18,6]#insertion sort 
+[22,27,16,2,18,6]#insertion sort ilk adım 
+[16,22,27,2,18,6]#isnertion sort ikinci adım
+[2,16,22,27,18,6]#3. adım
+[2,16,18,22,27,6]#4.adım
+[2,6,16,18,22,27]#5.adım son hali
+
+soru2=print('big(O)''su n''2(n kare)dir')
+
+soru_3=print('''Best case Time comp n,
+Avarage case n^2,
+Worst case time complexity n^2''')
+
+soru_4=print('18 avarage case kapsamındadır')
+
+soru_son=print('''[7,3,5,8,2,9,4,15,6]insertion sort
+[3,7,5,8,2,9,4,15,6]ilk adımı 
+[3,5,7,8,2,9,4,15,6]2.adımı
+[3,5,7,8,2,9,4,15,6]3.adımı
+[2,3,5,7,8,9,4,15,6]4.adım''')
